f080eaa6f94cda14ea42e11d8b06e452698bbc97
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-12-11 23:51:18 +0300

    Add home and about routes to Flask app

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def greeting():
+@app.route('/home')
+def home():
     return 'Welcome to my ShareItHub website!'
+
+@app.route('/about')
+def about_us():
+    return 'ShareItHub is a multi-user blogging and social posting platform built with Python and Flask.'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
