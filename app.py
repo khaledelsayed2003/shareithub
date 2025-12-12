8efb584a2f81b398b8a0757c416648d552b3114a
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-12-12 20:26:06 +0300

    feat(core): render home page with dummy posts using Jinja2

--- app.py
+++ app.py
@@ -2,10 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author':"Khaled Elsayed",
+        'title': "Python post!",
+        'content': "Flask is a lightweight Python web framework that helps you build websites and web applications easily.",
+        'date_posted': "December 12, 2025"
+    },
+    {
+        'author':"Jane Schafer",
+        'title': "Git post!",
+        'content': "VCS stands for Version Control System",
+        'date_posted': "December 13, 2025"
+    },
+    
+]
+
+
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
